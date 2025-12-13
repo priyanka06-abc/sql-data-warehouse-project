003fd6eefa7e6c7f00f9c04c243ab2f1129e7f17
priyanka06-abc <btech10043.22@bitmesra.ac.in>
2025-12-13 18:55:01 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,8 +1,36 @@
+/*
+=======================================================================
+Create Database and Schemas
+=======================================================================
+Script Purpose:
+   This script creates a new database name 'MysqlProject' after checking if it already exists.
+   If the database exists, it is dropped and recreated. Additionally the script sets up three schemas
+   within the database: 'bronze', 'silver', and 'gold'.
+
+WARNING:
+   Running this script drops the entire database 'MysqlProject' if it exists.
+   All the data in the database will be permanently deleted. Proceed with caution
+   and ensure you have proper backups before running this script.
+*/
+
 USE master;
+GO
 
-CREATE DATABASE DataWarehouse;
+--Drop and recreate the MysqlProject database
+IF EXISTS (SELECT 1 FROM sys.database WHERE name = 'MysqlProject')
+BEGIN
+  ALTER DATABASE MysqlProject SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE MysqlProject;
+END;
+GO
 
-USE DataWarehouse;
+  --Create the MysqlProject database
+
+CREATE DATABASE MysqlProject;
+
+USE MysqlProject;
+
+-- Create Schemas
 
 CREATE SCHEMA bronze;
 GO
